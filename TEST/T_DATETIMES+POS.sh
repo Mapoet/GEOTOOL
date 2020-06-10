36671ef170f2fc35162531f13455fd8247632715
niphy <mapoet&&qq.com>
2020-06-10 22:58:45 +0800

    update

--- TEST/T_DATETIMES+POS.sh
+++ TEST/T_DATETIMES+POS.sh
@@ -12,8 +12,8 @@ echo next day:`echo 2020 6 10 1|../BIN/DATETIMES 100`;
 echo next sec:`echo 2020 6 10 22 39 21 1|../BIN/DATETIMES 101`;
 echo before day:`echo 2020 6 10 1|../BIN/DATETIMES 190`;
 echo before sec:`echo 2020 6 10 22 39 21 1|../BIN/DATETIMES 191`;
-echo days between 2000 6 10 22 39 21 and 2020 6 10 22 39 21 :`echo 2020 6 10 2000 6 10 |../BIN/DATETIMES 180`;
-echo secs between 2000 6 10  and 2020 6 10 :`echo 2020 6 10 22 39 21 2000 6 10 22 39 21 |../BIN/DATETIMES 181`;
+echo days between 2000 6 10  and 2020 6 10 :`echo 2020 6 10 2000 6 10 |../BIN/DATETIMES 180`;
+echo secs between 2000 6 10 22 39 21 and 2020 6 10 22 39 21 :`echo 2020 6 10 22 39 21 2000 6 10 22 39 21 |../BIN/DATETIMES 181`;
 echo mjd2ydmd $mjd:`echo $mjd |../BIN/DATETIMES 310`;
 echo "test pos..."
 echo -1982905.001    4907862.100   3546474.5234|../BIN/POSCONVERT 31;
