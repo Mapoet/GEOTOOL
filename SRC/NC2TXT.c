3521a5f8bb1eea4281a83fac713f1dbce56db2ae
Mapoet <Mapoet.Niphy@gmail.com>
2020-06-29 11:28:40 +0800

    update

---
+++ SRC/NC2TXT.c
@@ -0,0 +1,64 @@
+
+#include <stdio.h>
+#include <netcdf.h>
+#include "CTIMES.h"
+#define R2D (180/3.1415926)
+
+int  main(int argc, char**argv){
+	int itype = -1, ntype[] = { 18, 6, 25, 12 };
+	const char names[][25][15] = { { "time", "caL1Snr", "pL1Snr", "pL2Snr", "xLeo", "yLeo", "zLeo", "xdLeo", "ydLeo", "zdLeo", "xGps", "yGps", "zGps", "xdGps", "ydGps", "zdGps", "exL1", "exL2" }, { "MSL_alt", "GEO_lat", "GEO_lon", "OCC_azi", "TEC_cal", "ELEC_dens" }, { "time", "caL1Snr", "pL1Snr", "pL2Snr", "xLeo", "yLeo", "zLeo", "xdLeo", "ydLeo", "zdLeo", "xGps", "yGps", "zGps", "xdGps", "ydGps", "zdGps", "exL1", "exL2", "exLC", "xmdl", "xmdldd", "xrng", "xmdl2", "xmdldd2", "xrng2" }, { "Lat", "Lon", "MSL_alt", "Ref", "Azim", "Pres", "Temp", "Bend_ang", "Opt_bend_ang", "Impact_parm", "Bend_ang_stdv", "Ref_stdv"/*, "OL_par", "OL_ipar", "OL_vec1", "OL_vec2", "OL_vec3", "OL_vec4"*/ } };
+	int ncid = 0, varid = 0, itemp = 0;
+	int i,j,inum = 0,wk;
+	double ep[6] = { 0 },mjd, **vals = NULL;
+	int rt = nc_open(argv[1], 0, &ncid), nw = 0;
+	if (argc != 2){
+		fprintf(stderr, "help:\n %s ncfile\n", argv[0]);
+		return 1;
+	}
+	if (strstr(argv[1], "ionPhs") != NULL)itype = 0;
+	if (strstr(argv[1], "ionPrf") != NULL)itype = 1;
+	if (strstr(argv[1], "atmPhs") != NULL)itype = 2;
+	if (strstr(argv[1], "atmPrf") != NULL)itype = 3;
+	if(itype==-1)return -2;
+	rt += nc_get_att_int(ncid, NC_GLOBAL, "year", &itemp);
+	ep[0] = itemp;
+	rt += nc_get_att_int(ncid, NC_GLOBAL, "month", &itemp);
+	ep[1] = itemp;
+	rt += nc_get_att_int(ncid, NC_GLOBAL, "day", &itemp);
+	ep[2] = itemp;
+	rt += nc_get_att_int(ncid, NC_GLOBAL, "hour", &itemp);
+	ep[3] = itemp;
+	rt += nc_get_att_int(ncid, NC_GLOBAL, "minute", &itemp);
+	ep[4] = itemp;
+	ymd2mjd(ep[0], ep[1], ep[2], ep[3], ep[4], ep[5], &mjd);
+	rt += nc_get_att_double(ncid, NC_GLOBAL, "second", ep + 5);
+	//fprintf(stdout, "time:%4.4d %2.2d %2.2d %2.2d %2.2d %4.1lf\n", (int)ep[0], (int)ep[1], (int)ep[2], (int)ep[3], (int)ep[4], (int)ep[5]);
+	vals = (double**)malloc(sizeof(double*)*ntype[itype]);
+	rt += nc_inq_varid(ncid, names[itype][0], &varid);
+	rt += nc_inq_dimlen(ncid, varid, &inum);
+	for (i = 0; i < ntype[itype]; i++){
+		//fprintf(stdout, "%20s",names[itype][i]);
+		vals[i] = (double*)malloc(sizeof(double)*inum);
+		rt += nc_inq_varid(ncid, names[itype][i], &varid);
+		rt += nc_get_var_double(ncid, varid, vals[i]);
+	}
+	rt+=nc_close(ncid);
+	if (rt != 0)return -1;
+	//fprintf(stdout, "\n");
+	for (i = 0; i < inum; i++)
+	{
+		for (j = 0; j < ntype[itype]; j++)
+		{
+			if (j == 0){
+				mjd2gpsws(mjd + vals[j][i] / 86400, &wk, &ep[5]);
+				fprintf(stdout, "%4.4d %15.3lf", wk,ep[5]);
+			}
+			else
+				fprintf(stdout, "%20.5lf", vals[j][i]);
+		}
+		fprintf(stdout, "\n");
+	}
+	for (i = 0; i < ntype[itype]; i++)free(vals[i]);
+	free(vals);
+	return 0;
+}
--- SRC/XFORM.c
+++ SRC/XFORM.c
@@ -309,6 +309,172 @@ extern void state2ele(const double *state, double *ele)
 	ele[4]=omg*180.0/M_PI;
 	ele[5]=M*180.0/M_PI;
 }
+/*
+!-------------------------------------------------------------------------
+!
+! Writes TLE-records to an output-file.
+! The TLE_Data-fields nDO20, nDDO60 and BSTAR are ignored.
+!
+! Input: toFile = name of outputfile
+!    NumWrite = number of records to write
+!    TLE_Data = holds 'NumWrite' TLE information records to be written.
+!    Replace = optional; Replace existing outputfile? (default: append).
+! Output: IOSTAT = optional; returns I/O status, 0 if no error occured.
+!
+SUBROUTINE Write_TLE_Rec(toFile, NumWrite, TLE_Data, &
+              IOSTAT, Replace)
+   CHARACTER (LEN=*), INTENT(IN) :: toFile
+   INTEGER, INTENT(IN) :: NumWrite
+   TYPE (TLE_Set), INTENT(IN) :: TLE_Data(MAXSat)
+   INTEGER, OPTIONAL, INTENT(OUT) :: IOSTAT
+   LOGICAL, OPTIONAL, INTENT(IN) :: Replace
+
+   LOGICAL :: Opened, Existing
+   INTEGER :: unit, ios, i
+   CHARACTER (LEN=10) :: Status, Position
+
+   Status='old'
+   Position='append'
+   IF (PRESENT(Replace)) THEN
+      IF (Replace) Status = 'replace'
+   ENDIF
+   INQUIRE(FILE=TRIM(toFile), EXIST=Existing, OPENED=Opened)
+   IF (.NOT. Existing) Status = 'replace'
+   IF (.NOT. Opened) THEN
+      PRINT *,'Opening file... '
+
+      call getLun_s(unit)
+      OPEN(unit, FILE=TRIM(toFile), IOSTAT=ios, ERR=99, STATUS=Status, &
+            POSITION=Position, ACCESS='sequential', FORM='formatted', &
+            BLANK='zero')
+   ENDIF
+
+   DO i = 1, NumWrite
+      WRITE(unit, 52, IOSTAT=ios, ERR=99) TRIM(ADJUSTL(TLE_Data(i)%SatID))
+      ! Attention: nDO20, nDDO60 and BSTAR are zero.
+      WRITE(unit, 54, IOSTAT=ios, ERR=99) i, TLE_Data(i)%IntDesig, TLE_Data(i)%EPOCH
+      WRITE(unit, 56, IOSTAT=ios, ERR=99) i, TLE_Data(i)%i0, &
+           TLE_Data(i)%Node0, TLE_Data(i)%e0, TLE_Data(i)%omega0, &
+           TLE_Data(i)%M0, TLE_Data(i)%n0
+      WRITE (*,*) 'Processed.'
+   ENDDO
+
+   CLOSE(unit, IOSTAT=ios)
+   call freeLun_s(unit)
+
+   GOTO 99
+
+52 FORMAT (A)
+54 FORMAT ('1',1x,I5,2x,A8,1x,F14.8,2x,'.00000000',2(2x,'00000-0'),&
+           1x,'0',2x,'0010')
+56 FORMAT ('2',1x,I5,2(1X,F8.4),F8.7,0P,2(1x,F8.4),1X,F11.8,&
+           '     ','0',T26,' ')
+
+99 IF (PRESENT(IOSTAT)) IOSTAT = ios
+END SUBROUTINE Write_TLE_Rec
+!-------------------------------------------------------------------------
+!
+! TLEtoKepler converts two-line-element sets into Kepler-element-records (for
+! use within KeplerOrbit or CircularOrbit).
+!
+! Input: TLE
+! Output: Kepler Element set.
+!
+SUBROUTINE TLEtoKepler(TLE, KeplerElem)
+   TYPE (TLE_Set), INTENT(INOUT) :: TLE
+   TYPE (Kepler_Elements), INTENT(OUT) :: KeplerElem
+
+   REAL( DP ) :: dummy
+
+   ! calculate semimajor axis (out of TLE-set) if necessary:
+   CALL ModifyTLESet(TLE)
+
+   KeplerElem%New_Elements = .TRUE.
+   KeplerElem%a = TLE%a
+   KeplerElem%e = TLE%e0
+   KeplerElem%i = TLE%i0 * RAD_TO_DEG
+   KeplerElem%Node0 = TLE%Node0 * RAD_TO_DEG
+   KeplerElem%omega0 = TLE%omega0 * RAD_TO_DEG
+   KeplerElem%EPOCH = TLE%EPOCH
+   CALL ModifyKeplerSet(KeplerElem)
+   ! calculate epoch of perigee passage (mean anomaly = 0)
+   dummy = TLE%M0/KeplerElem%n_pert
+   KeplerElem%EPOCH = KeplerElem%EPOCH - dummy
+   KeplerElem%Node0_rad = MOD(KeplerElem%Node0_rad - dummy * &
+        KeplerElem%Node_dot, TWO_PI)
+   IF (KeplerElem%Node0_rad < 0.0_DP) &
+        KeplerElem%Node0_rad = KeplerElem%Node0_rad + TWO_PI
+   KeplerElem%omega0_rad = MOD(KeplerElem%omega0_rad - dummy * &
+        KeplerElem%omega_dot, TWO_PI)
+   IF (KeplerElem%omega0_rad < 0.0_DP) &
+        KeplerElem%omega0_rad = KeplerElem%omega0_rad + TWO_PI
+   KeplerElem%Node0 = KeplerElem%Node0_rad * RAD_TO_DEG
+   KeplerElem%omega0 = KeplerElem%omega0_rad * RAD_TO_DEG
+END SUBROUTINE TLEtoKepler
+
+
+
+!-------------------------------------------------------------------------
+!
+! KeplerToTLE converts a set of Kepler elements to a TLE-set.
+!
+! Input: Kepler = Kepler elements to be converted
+! Output: TLE = two-line-elements corresponding to Kepler elements.
+!
+SUBROUTINE KeplertoTLE(Kepler, TLE)
+   TYPE (Kepler_Elements), INTENT(INOUT) :: Kepler
+   TYPE (TLE_Set), INTENT(OUT) :: TLE
+
+   CALL ModifyKeplerSet(Kepler)
+   TLE%M0 = 0.0_DP               ! "mean" mean anomaly at epoch
+   TLE%Node0 = Kepler%Node0   ! "mean" longitude of ascending node at epoch
+   TLE%omega0 = Kepler%omega0 ! "mean" argument of perigee at epoch
+   TLE%e0 = Kepler%e          ! "mean" eccentricity at epoch
+   TLE%i0 = Kepler%i          ! "mean" inclination at epoch
+   TLE%nDO20 = 0.0_DP         ! 1/2 of the First Time Derivative of Mean
+                              ! Motion at epoch
+   TLE%nDDO60 = 0.0_DP        ! 1/6 of the Second Time Derivative of Mean
+                              ! Motion at epoch
+   TLE%BSTAR = 0.0_DP         ! the SGP4 type drag coefficient is zero
+   TLE%EPOCH = Kepler%Epoch   ! the epoch (modified julian date)
+   TLE%n0 = ModifiedMeanMotion(Kepler%n, TLE%i0, TLE%e0)/TWO_PI   ! SGP type
+                              ! "mean" mean motion at epoch
+   TLE%New_Elements = .TRUE.
+END SUBROUTINE KeplertoTLE
+
+
+
+!-------------------------------------------------------------------------
+!
+! ModifiedMeanMotion calculates the modified mean motion (saved in the
+! TLE files and later used by routine SGPOrbit) out of the original
+! mean motion.
+! Input: n = original mean motion [radians/day]
+!   i = inclination [degrees]
+!   e = eccentricity
+! Output: modified mean motion [radians/day]
+!
+REAL( DP ) FUNCTION ModifiedMeanMotion(n, i, e)
+   REAL( DP ), INTENT(IN) :: n, i, e
+
+   REAL( DP ) :: COSIO, DFAC, A2, D2, n1, A1, D1, AO, DO, n0
+
+   COSIO = COS(i*DE2RA)
+   DFAC = (3.0_DP*COSIO*COSIO - 1.0_DP)/(1.0_DP - e*e)**1.5_DP
+   A2 = (XKE/(n/XMNPDA))**TOTHIRD
+   D2 = CK2*1.5_DP/A2/A2*DFAC
+   n1 = n * (1.0_DP + D2)               ! 1st guess for modified n
+   A1 = (XKE/(n1/XMNPDA))**TOTHIRD
+   D1 = CK2*1.5_DP/A1/A1*DFAC
+   AO = A1*(1.0_DP - 1.0_DP/3.0_DP*D1 - D1*D1 - 134.0_DP/81.0_DP*D1*D1*D1)
+   DO = CK2*1.5_DP/AO/AO*DFAC
+   n0 = n * (1.0_DP + DO)               ! cf. [1] (KELSO report), p.21
+   ModifiedMeanMotion = n0
+END FUNCTION ModifiedMeanMotion
+
+*/
+
+
 /*-------------------------------------------------------------------------------
 % [system] : GpsTools
 % [module] : satellite azimuth/elevation angle
@@ -325,7 +491,7 @@ extern void state2ele(const double *state, double *ele)
 extern void satazel(const double *spos, const double *rpos, double *azel)
 {
 	double gpos[3],rs[3],rss[3],E[9];
-	EcefToGeod(rpos,gpos,E);
+	ecef2geod(rpos,gpos,E);
 	rs[0]=spos[0]-rpos[0];
 	rs[1]=spos[1]-rpos[1];
 	rs[2]=spos[2]-rpos[2];
@@ -334,7 +500,255 @@ extern void satazel(const double *spos, const double *rpos, double *azel)
 	azel[1]=asin(rss[2]/NORM(rs));
 }
 
+#define DE2RA       0.174532925E-1
+#define E6A         1.E-6
+#define PIO2        1.57079633
+#define QO          120.0
+#define SO          78.0
+#define TOTHRD      0.66666667
+#define TWOPI       6.2831853
+#define X3PIO2      4.71238898
+#define XJ2         1.082616E-3
+#define XJ3         -0.253881E-5
+#define XJ4         -1.65597E-6
+#define XKE         0.743669161E-1
+#define XKMPER      6378.135
+#define XMNPDA      1440.0
+#define AE          1.0
+#define CK2         5.413080E-4         /* = 0.5*XJ2*AE*AE */
+#define CK4         0.62098875E-6       /* = -0.375*XJ4*AE*AE*AE*AE */
+#define QOMS2T      1.88027916E-9       /* = pow((QO-SO)*AE/XKMPER,4.0) */
+#define S           1.01222928          /* = AE*(1.0+SO/XKMPER) */
 
+static void SGP4_STR3(double tsince, const /*tled_t *data*/double *data, double *rs)
+{
+    double xnodeo,omegao,xmo,eo,xincl,xno,xndt2o,xndd6o,bstar;
+    double a1,cosio,theta2,x3thm1,eosq,betao2,betao,del1,ao,delo,xnodp,aodp,s4;
+    double qoms24,perige,pinvsq,tsi,eta,etasq,eeta,psisq,coef,coef1,c1,c2,c3,c4;
+    double c5,sinio,a3ovk2,x1mth2,theta4,xmdot,x1m5th,omgdot,xhdot1,xnodot;
+    double omgcof,xmcof,xnodcf,t2cof,xlcof,aycof,delmo,sinmo,x7thm1,c1sq,d2,d3;
+    double d4,t3cof,t4cof,t5cof,xmdf,omgadf,xnoddf,omega,xmp,tsq,xnode,delomg;
+    double delm,tcube,tfour,a,e,xl,beta,xn,axn,xll,aynl,xlt,ayn,capu,sinepw;
+    double cosepw,epw,ecose,esine,elsq,pl,r,rdot,rfdot,betal,cosu,sinu,u,sin2u;
+    double cos2u,rk,uk,xnodek,xinck,rdotk,rfdotk,sinuk,cosuk,sinik,cosik,sinnok;
+    double cosnok,xmx,xmy,ux,uy,uz,vx,vy,vz,x,y,z,xdot,ydot,zdot;
+    double temp,temp1,temp2,temp3,temp4,temp5,temp6,tempa,tempe,templ;
+    int i,isimp;
+    
+    xnodeo=data[0];//data->OMG*DE2RA;
+    omegao=data[1];//data->omg*DE2RA;
+    xmo=data[2];//data->M*DE2RA;
+    xincl=data[3];//data->inc*DE2RA;
+    temp=TWOPI/XMNPDA/XMNPDA;
+    xno=data[4];//data->n*temp*XMNPDA;
+    xndt2o=data[5];//data->ndot*temp;
+    xndd6o=data[6];//data->nddot*temp/XMNPDA;
+    bstar=data[7];//data->bstar/AE;
+    eo=data[8];//data->ecc;
+    /*
+    * recover original mean motion (xnodp) and semimajor axis (aodp)
+    * from input elements
+    */
+    a1=pow(XKE/xno,TOTHRD);
+    cosio=cos(xincl);
+    theta2=cosio*cosio;
+    x3thm1=3.0*theta2-1.0;
+    eosq=eo*eo;
+    betao2=1.0-eosq;
+    betao=sqrt(betao2);
+    del1=1.5*CK2*x3thm1/(a1*a1*betao*betao2);
+    ao=a1*(1.0-del1*(0.5*TOTHRD+del1*(1.0+134.0/81.0*del1)));
+    delo=1.5*CK2*x3thm1/(ao*ao*betao*betao2);
+    xnodp=xno/(1.0+delo);
+    aodp=ao/(1.0-delo);
+    /*
+    * initialization
+    * for perigee less than 220 kilometers, the isimp flag is set and
+    * the equations are truncated to linear variation in sqrt a and
+    * quadratic variation in mean anomaly. also, the c3 term, the
+    * delta omega term, and the delta m term are dropped.
+    */
+    isimp=0;
+    if ((aodp*(1.0-eo)/AE)<(220.0/XKMPER+AE)) isimp=1;
+    
+    /* for perigee below 156 km, the values of s and qoms2t are altered */
+    s4=S;
+    qoms24=QOMS2T;
+    perige=(aodp*(1.0-eo)-AE)*XKMPER;
+    if (perige<156.0) {
+        s4=perige-78.0;
+        if (perige<=98.0) s4=20.0;
+        qoms24=pow((120.0-s4)*AE/XKMPER,4.0);
+        s4=s4/XKMPER+AE;
+    }
+    pinvsq=1.0/(aodp*aodp*betao2*betao2);
+    tsi=1.0/(aodp-s4);
+    eta=aodp*eo*tsi;
+    etasq=eta*eta;
+    eeta=eo*eta;
+    psisq=fabs(1.0-etasq);
+    coef=qoms24*pow(tsi,4.0);
+    coef1=coef/pow(psisq,3.5);
+    c2=coef1*xnodp*(aodp*(1.0+1.5*etasq+eeta*(4.0+etasq))+0.75*
+            CK2*tsi/psisq*x3thm1*(8.0+3.0*etasq*(8.0+etasq)));
+    c1=bstar*c2;
+    sinio=sin(xincl);
+    a3ovk2=-XJ3/CK2*pow(AE,3.0);
+    c3=coef*tsi*a3ovk2*xnodp*AE*sinio/eo;
+    x1mth2=1.0-theta2;
+    c4=2.0*xnodp*coef1*aodp*betao2*(eta*
+            (2.0+0.5*etasq)+eo*(0.5+2.0*etasq)-2.0*CK2*tsi/
+            (aodp*psisq)*(-3.0*x3thm1*(1.0-2.0*eeta+etasq*
+            (1.5-0.5*eeta))+0.75*x1mth2*(2.0*etasq-eeta*
+            (1.0+etasq))*cos(2.0*omegao)));
+    c5=2.0*coef1*aodp*betao2*(1.0+2.75*(etasq+eeta)+eeta*etasq);
+    theta4=theta2*theta2;
+    temp1=3.0*CK2*pinvsq*xnodp;
+    temp2=temp1*CK2*pinvsq;
+    temp3=1.25*CK4*pinvsq*pinvsq*xnodp;
+    xmdot=xnodp+0.5*temp1*betao*x3thm1+0.0625*temp2*betao*
+            (13.0-78.0*theta2+137.0*theta4);
+    x1m5th=1.0-5.0*theta2;
+    omgdot=-0.5*temp1*x1m5th+0.0625*temp2*(7.0-114.0*theta2+
+            395.0*theta4)+temp3*(3.0-36.0*theta2+49.0*theta4);
+    xhdot1=-temp1*cosio;
+    xnodot=xhdot1+(0.5*temp2*(4.0-19.0*theta2)+2.0*temp3*(3.0-
+            7.0*theta2))*cosio;
+    omgcof=bstar*c3*cos(omegao);
+    xmcof=-TOTHRD*coef*bstar*AE/eeta;
+    xnodcf=3.5*betao2*xhdot1*c1;
+    t2cof=1.5*c1;
+    xlcof=0.125*a3ovk2*sinio*(3.0+5.0*cosio)/(1.0+cosio);
+    aycof=0.25*a3ovk2*sinio;
+    delmo=pow(1.0+eta*cos(xmo),3.0);
+    sinmo=sin(xmo);
+    x7thm1=7.0*theta2-1.0;
+    
+    if (isimp!=1) {
+        c1sq=c1*c1;
+        d2=4.0*aodp*tsi*c1sq;
+        temp=d2*tsi*c1/3.0;
+        d3=(17.0*aodp+s4)*temp;
+        d4=0.5*temp*aodp*tsi*(221.0*aodp+31.0*s4)*c1;
+        t3cof=d2+2.0*c1sq;
+        t4cof=0.25*(3.0*d3+c1*(12.0*d2+10.0*c1sq));
+        t5cof=0.2*(3.0*d4+12.0*c1*d3+6.0*d2*d2+15.0*c1sq*(2.0*d2+c1sq));
+    }
+    else {
+        d2=d3=d4=t3cof=t4cof=t5cof=0.0;
+    }
+    /* update for secular gravity and atmospheric drag */
+    xmdf=xmo+xmdot*tsince;
+    omgadf=omegao+omgdot*tsince;
+    xnoddf=xnodeo+xnodot*tsince;
+    omega=omgadf;
+    xmp=xmdf;
+    tsq=tsince*tsince;
+    xnode=xnoddf+xnodcf*tsq;
+    tempa=1.0-c1*tsince;
+    tempe=bstar*c4*tsince;
+    templ=t2cof*tsq;
+    if (isimp==1) {
+        delomg=omgcof*tsince;
+        delm=xmcof*(pow(1.0+eta*cos(xmdf),3.0)-delmo);
+        temp=delomg+delm;
+        xmp=xmdf+temp;
+        omega=omgadf-temp;
+        tcube=tsq*tsince;
+        tfour=tsince*tcube;
+        tempa=tempa-d2*tsq-d3*tcube-d4*tfour;
+        tempe=tempe+bstar*c5*(sin(xmp)-sinmo);
+        templ=templ+t3cof*tcube+tfour*(t4cof+tsince*t5cof);
+    }
+    a=aodp*pow(tempa,2.0);
+    e=eo-tempe;
+    xl=xmp+omega+xnode+xnodp*templ;
+    beta=sqrt(1.0-e*e);
+    xn=XKE/pow(a,1.5);
+    
+    /* long period periodics */
+    axn=e*cos(omega);
+    temp=1.0/(a*beta*beta);
+    xll=temp*xlcof*axn;
+    aynl=temp*aycof;
+    xlt=xl+xll;
+    ayn=e*sin(omega)+aynl;
+    
+    /* solve keplers equation */
+    capu=fmod(xlt-xnode,TWOPI);
+    temp2=capu;
+    for (i=0;i<10;i++) {
+        sinepw=sin(temp2);
+        cosepw=cos(temp2);
+        temp3=axn*sinepw;
+        temp4=ayn*cosepw;
+        temp5=axn*cosepw;
+        temp6=ayn*sinepw;
+        epw=(capu-temp4+temp3-temp2)/(1.0-temp5-temp6)+temp2;
+        if (fabs(epw-temp2)<=E6A) break;
+        temp2=epw;
+    }
+    /* short period preliminary quantities */
+    ecose=temp5+temp6;
+    esine=temp3-temp4;
+    elsq=axn*axn+ayn*ayn;
+    temp=1.0-elsq;
+    pl=a*temp;
+    r=a*(1.0-ecose);
+    temp1=1.0/r;
+    rdot=XKE*sqrt(a)*esine*temp1;
+    rfdot=XKE*sqrt(pl)*temp1;
+    temp2=a*temp1;
+    betal=sqrt(temp);
+    temp3=1.0/(1.0+betal);
+    cosu=temp2*(cosepw-axn+ayn*esine*temp3);
+    sinu=temp2*(sinepw-ayn-axn*esine*temp3);
+    u=atan2(sinu,cosu);
+    sin2u=2.0*sinu*cosu;
+    cos2u=2.0*cosu*cosu-1.0;
+    temp=1.0/pl;
+    temp1=CK2*temp;
+    temp2=temp1*temp;
+    
+    /* update for short periodics */
+    rk=r*(1.0-1.5*temp2*betal*x3thm1)+0.5*temp1*x1mth2*cos2u;
+    uk=u-0.25*temp2*x7thm1*sin2u;
+    xnodek=xnode+1.5*temp2*cosio*sin2u;
+    xinck=xincl+1.5*temp2*cosio*sinio*cos2u;
+    rdotk=rdot-xn*temp1*x1mth2*sin2u;
+    rfdotk=rfdot+xn*temp1*(x1mth2*cos2u+1.5*x3thm1);
+    
+    /* orientation vectors */
+    sinuk=sin(uk);
+    cosuk=cos(uk);
+    sinik=sin(xinck);
+    cosik=cos(xinck);
+    sinnok=sin(xnodek);
+    cosnok=cos(xnodek);
+    xmx=-sinnok*cosik;
+    xmy=cosnok*cosik;
+    ux=xmx*sinuk+cosnok*cosuk;
+    uy=xmy*sinuk+sinnok*cosuk;
+    uz=sinik*sinuk;
+    vx=xmx*cosuk-cosnok*sinuk;
+    vy=xmy*cosuk-sinnok*sinuk;
+    vz=sinik*cosuk;
+    
+    /* position and velocity */
+    x=rk*ux;
+    y=rk*uy;
+    z=rk*uz;
+    xdot=rdotk*ux+rfdotk*vx;
+    ydot=rdotk*uy+rfdotk*vy;
+    zdot=rdotk*uz+rfdotk*vz;
+    
+    rs[0]=x*XKMPER/AE*1E3; /* (m) */
+    rs[1]=y*XKMPER/AE*1E3;
+    rs[2]=z*XKMPER/AE*1E3;
+    rs[3]=xdot*XKMPER/AE*XMNPDA/86400.0*1E3; /* (m/s) */
+    rs[4]=ydot*XKMPER/AE*XMNPDA/86400.0*1E3;
+    rs[5]=zdot*XKMPER/AE*XMNPDA/86400.0*1E3;
+}
 /*------------------------------------------------------------------------------
 % [system] : GpsTools
 % [module] : spheric harmonic functions
